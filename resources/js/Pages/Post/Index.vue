4b25a1392ea85437cbf0c927ba5756ed00d925c8
Hsieh Chang Wei <jacky790125@gmail.com>
2025-12-26 23:03:14 +0800

    feat(ui): 新增文章列表頁面元件

---
+++ resources/js/Pages/Post/Index.vue
@@ -0,0 +1,30 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps({
+    'posts': Array,
+});
+</script>
+
+<template>
+    <AppLayout title="Posts">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Posts
+            </h2>
+        </template>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <h1 class="text-2xl font-bold mb-4">Posts</h1>
+                    <ul>
+                        <li v-for="post in posts" :key="post.id" class="mb-2">
+                            <h2 class="text-xl font-semibold">{{ post.title }}</h2>
+                            <p>{{ post.content }}</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
