50a30d5c906b834d2aea3a12b294855283a1d338
Hsieh Chang Wei <jacky790125@gmail.com>
2025-12-26 23:32:53 +0800

    style(posts): 調整文章列表頁面元件的排版和樣式

--- resources/js/Pages/Post/Index.vue
+++ resources/js/Pages/Post/Index.vue
@@ -17,10 +17,9 @@ defineProps({
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                     <h1 class="text-2xl font-bold mb-4">Posts</h1>
-                    <ul>
-                        <li v-for="post in posts" :key="post.id" class="mb-2">
-                            <h2 class="text-xl font-semibold">{{ post.title }}</h2>
-                            <p>{{ post.content }}</p>
+                    <ul class="divide-y">
+                        <li v-for="post in posts" :key="post.id" class="px-2 py-4">
+                            <span class="font-bold text-lg">{{ post.title }}</span>
                         </li>
                     </ul>
                 </div>
