a645b5e05878f91bcc37a18bfeee6df0c7da02e4
Hsieh Chang Wei <jacky790125@gmail.com>
2025-12-29 00:21:45 +0800

    refactor(pagination): 更新分頁元件以使用 Link 組件並改善樣式

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -26,13 +26,16 @@
             </div>
             <div>
                 <nav class="isolate inline-flex -space-x-px rounded-md" aria-label="Pagination">
-                    <!-- Current: "z-10 text-white focus-visible:outline-2 focus-visible:outline-offset-2 bg-indigo-500 focus-visible:outline-indigo-500", Default: "inset-ring focus:outline-offset-0 text-gray-200 inset-ring-gray-700 hover:bg-white/5" -->
-                    <a v-for="link in meta.links" :href="link.url" v-html="link.label"
-                        class="relative z-10 inline-flex items-center first-of-type:rounded-l-md last-of-type:rounded-r-md bg-indigo-500 px-3 py-2 text-sm font-semibold"
-                        :class="{
-                            'bg-indigo-500 text-white focus:z-20 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500': link.active,
-                            'inset-ring focus:outline-offset-0 inset-ring-gray-700 hover:bg-white/5': !link.active
-                        }"></a>
+                    <template v-for="link in meta.links" :key="link.label">
+                        <Link v-if="link.url" :href="link.url" v-html="link.label"
+                            class="relative inline-flex items-center first-of-type:rounded-l-md last-of-type:rounded-r-md px-3 py-2 text-sm font-semibold focus:z-20 focus:outline-offset-0"
+                            :class="{
+                                'z-10 bg-indigo-500 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500': link.active,
+                                'text-gray-500 inset-ring inset-ring-gray-700 hover:bg-white/5': !link.active
+                            }" />
+                        <span v-else v-html="link.label"
+                            class="relative inline-flex items-center first-of-type:rounded-l-md last-of-type:rounded-r-md px-3 py-2 text-sm font-semibold text-gray-200 inset-ring inset-ring-gray-700"></span>
+                    </template>
                 </nav>
             </div>
         </div>
@@ -40,15 +43,9 @@
 </template>
 
 <script setup>
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/20/solid'
+import { Link } from '@inertiajs/vue3';
 
 defineProps({
     meta: Object,
 })
-
-const items = [
-    { id: 1, title: 'Back End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
-    { id: 2, title: 'Front End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
-    { id: 3, title: 'User Interface Designer', department: 'Design', type: 'Full-time', location: 'Remote' },
-]
 </script>
