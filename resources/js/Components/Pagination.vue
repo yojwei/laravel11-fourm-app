8405872e76560401dfdf42bd89c80cb32782d577
Hsieh Chang Wei <jacky790125@gmail.com>
2025-12-29 13:15:55 +0800

    refactor(pagination): 將分頁元件中的 a 標籤替換為 Link 組件

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="flex items-center justify-between border-t border-white/10 px-4 py-3 sm:px-6">
         <div class="flex flex-1 justify-between sm:hidden">
-            <a href="#"
-                class="relative inline-flex items-center rounded-md border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium text-gray-200 hover:bg-white/10">Previous</a>
-            <a href="#"
-                class="relative ml-3 inline-flex items-center rounded-md border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium text-gray-200 hover:bg-white/10">Next</a>
+            <Link :href="meta.prev_page_url"
+                class="relative inline-flex items-center rounded-md border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium hover:bg-white/10">
+                Previous</Link>
+            <Link :href="meta.next_page_url"
+                class="relative ml-3 inline-flex items-center rounded-md border border-white/10 bg-white/5 px-4 py-2 text-sm font-medium hover:bg-white/10">
+                Next</Link>
         </div>
         <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
             <div>
@@ -32,7 +34,7 @@
                             :class="{
                                 'z-10 bg-indigo-500 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500': link.active,
                                 'text-gray-500 inset-ring inset-ring-gray-700 hover:bg-white/5': !link.active
-                            }" />
+                            }" preserve-scroll />
                         <span v-else v-html="link.label"
                             class="relative inline-flex items-center first-of-type:rounded-l-md last-of-type:rounded-r-md px-3 py-2 text-sm font-semibold text-gray-200 inset-ring inset-ring-gray-700"></span>
                     </template>
