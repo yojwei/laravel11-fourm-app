f11f0d580561541554c96ab376e9999bd26e27c2
Hsieh Chang Wei <jacky790125@gmail.com>
2025-12-29 13:15:49 +0800

    fix(Index.vue): 修正標題的拼寫錯誤

--- resources/js/Pages/Post/Index.vue
+++ resources/js/Pages/Post/Index.vue
@@ -10,14 +10,13 @@ defineProps({
 <template>
     <AppLayout title="Posts">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tigh">
                 Posts
             </h2>
         </template>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <h1 class="text-2xl font-bold mb-4">Posts</h1>
                     <ul class="divide-y">
                         <li v-for="post in posts.data" :key="post.id" class="px-2 py-4">
                             <span class="font-bold text-lg">{{ post.title }}</span>
