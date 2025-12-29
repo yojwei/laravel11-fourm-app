2bfbe663bb429ef98b49e40b4147041a2757a49d
Hsieh Chang Wei <jacky790125@gmail.com>
2025-12-29 20:20:15 +0800

    feat(ui): 新增 Container 元件
    新增通用的 Container 元件，封裝標準的頁面容器布局。
    該元件提供 py-12 的垂直間距和 max-w-7xl 的最大寬度限制，
    並支援響應式的水平內邊距。

---
+++ resources/js/Components/Container.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <slot></slot>
+        </div>
+    </div>
+</template>
