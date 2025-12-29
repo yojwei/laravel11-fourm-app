2780a0edc24d394ccdceeec74206fd176432f553
Hsieh Chang Wei <jacky790125@gmail.com>
2025-12-29 20:40:02 +0800

    refactor(database): 精簡 DatabaseSeeder 的用戶與評論生成邏輯

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,24 +17,22 @@ class DatabaseSeeder extends Seeder
     {
         // 建立 10 個測試用戶
         $users = User::factory(10)->create();
+
+        // 將第一個用戶更新為 Test User
+        $users->first()->update([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
         // 建立 200 篇貼文，並將其關聯到現有的用戶
         $posts = Post::factory(200)
             ->recycle($users)
             ->create();
 
         // 建立 500 則評論，並將其關聯到現有的用戶和貼文
-        Comment::class::factory(500)
+        Comment::factory(500)
             ->recycle($users)
             ->recycle($posts)
             ->create();
-
-        // 建立測試用戶，擁有 50 篇貼文和 150 則評論
-        User::factory()
-            ->has(Post::factory(50))
-            ->has(Comment::factory(150)->recycle($posts))
-            ->create([
-                'name' => 'Test User',
-                'email' => 'test@example.com',
-            ]);
     }
 }
