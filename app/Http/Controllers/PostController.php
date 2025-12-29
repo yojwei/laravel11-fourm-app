b3b2c5403a6814ff65d3848d69b1004f65525457
Hsieh Chang Wei <jacky790125@gmail.com>
2025-12-29 16:29:41 +0800

    feat(post): 新增 PostResource 與改進文章排序
    - 新增 PostResource API 資源類別，用於轉換文章資料
    - 修改 PostController::index() 以使用 PostResource::collection()
    - 改進排序邏輯，優先以發佈時間排序，其次以 ID 排序

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
     public function index()
     {
         return inertia('Post/Index', [
-            'posts' => PostResource::collection(Post::paginate()),
+            'posts' => PostResource::collection(Post::latest()->latest('id')->paginate()),
         ]);
     }
 }
