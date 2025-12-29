5289fc2b21a283d1c8dc83c5e562a45369145427
Hsieh Chang Wei <jacky790125@gmail.com>
2025-12-29 20:20:41 +0800

    feat(posts): 改進文章詳情頁面 UI 與內容呈現
    使用 Container 元件重構頁面布局，
    新增行號計算邏輯以支援多行文章內容正確顯示，
    提升標題字體大小和整體視覺層級，
    增強文章發布資訊的排版和對比度。

--- resources/js/Pages/Post/Show.vue
+++ resources/js/Pages/Post/Show.vue
@@ -2,6 +2,7 @@
 import { computed } from 'vue';
 import AppLayout from '@/Layouts/AppLayout.vue';
 import { DateTime } from "luxon"
+import Container from '@/Components/Container.vue';
 
 const props = defineProps({
     post: Object,
@@ -11,16 +12,29 @@ const formattedDate = computed(() => {
     return DateTime.fromISO(props.post.created_at).toRelative({ locale: 'en' });
 });
 
+const lines = computed(() => {
+    return props.post.body ? props.post.body.split(/\r?\n/) : [];
+});
+
 </script>
 
 <template>
     <AppLayout :title="post.title">
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <h1 class="text-xl font-bold">{{ post.title }}</h1>
-                <span>{{ formattedDate }} by {{ post.user.name }}</span>
-                <article>{{ post.body }}</article>
+        <Container>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <h1 class="text-3xl font-bold px-6 pt-4">{{ post.title }}</h1>
+                <span class="block px-6 pb-4 text-gray-500 text-right">{{ formattedDate }}
+                    <span class="font-bold text-gray-700">by {{ post.user.name }}</span>
+                </span>
+
+                <hr />
+
+                <article class="px-6 py-6">
+                    <div v-for="(line, index) in lines" :key="index" class="indent-4 min-h-[1.5em] text-base/6">
+                        {{ line }}
+                    </div>
+                </article>
             </div>
-        </div>
+        </Container>
     </AppLayout>
 </template>
