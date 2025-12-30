24132cdb359324e8ef06b76572dc2f23cff603df
Hsieh Chang Wei <jacky790125@gmail.com>
2025-12-30 17:10:54 +0800

    feat(post): 新增貼文評論顯示功能
    - PostController show 方法返回評論資源
    - CommentResource 使用 whenLoaded 改進關聯資源載入
    - Post Show 頁面新增評論顯示區域
    - 新增 Comment 組件用於顯示單筆評論

--- resources/js/Pages/Post/Show.vue
+++ resources/js/Pages/Post/Show.vue
@@ -1,16 +1,17 @@
 <script setup>
 import { computed } from 'vue';
 import AppLayout from '@/Layouts/AppLayout.vue';
-import { DateTime } from "luxon"
 import Container from '@/Components/Container.vue';
+import Pagination from '@/Components/Pagination.vue';
+import { relativeDate } from "@/Utilities/date.js";
+import Comment from '@/Components/Comment.vue';
 
 const props = defineProps({
     post: Object,
+    comments: Object
 })
 
-const formattedDate = computed(() => {
-    return DateTime.fromISO(props.post.created_at).toRelative({ locale: 'en' });
-});
+const formattedDate = (date) => { return relativeDate(date); };
 
 const lines = computed(() => {
     return props.post.body ? props.post.body.split(/\r?\n/) : [];
@@ -23,7 +24,7 @@ const lines = computed(() => {
         <Container>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <h1 class="text-3xl font-bold px-6 pt-4">{{ post.title }}</h1>
-                <span class="block px-6 pb-4 text-gray-500 text-right">{{ formattedDate }}
+                <span class="block px-6 pb-4 text-gray-500 text-right">{{ formattedDate(post.created_at) }}
                     <span class="font-bold text-gray-700">by {{ post.user.name }}</span>
                 </span>
 
@@ -34,6 +35,17 @@ const lines = computed(() => {
                         {{ line }}
                     </div>
                 </article>
+
+                <div class="px-6 pb-6">
+                    <h2 class="text-lg font-bold">Comments</h2>
+
+                    <ul class="divide-y">
+                        <li v-for="comment in comments.data" :key="comment.id" class="py-2 hover:bg-gray-100">
+                            <Comment :comment="comment" />
+                        </li>
+                    </ul>
+                    <Pagination :meta="comments.meta" class="mt-6 flex justify-center" />
+                </div>
             </div>
         </Container>
     </AppLayout>
