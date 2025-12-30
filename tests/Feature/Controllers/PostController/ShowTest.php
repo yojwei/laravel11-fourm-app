71f964cf1e5822e954ffd57d1357eaf381f2d6bc
Hsieh Chang Wei <jacky790125@gmail.com>
2025-12-30 17:11:10 +0800

    test: 新增貼文評論傳遞的測試
    - 驗證 PostController show 方法正確傳遞評論資源
    - 測試評論資源集合的分頁功能

--- tests/Feature/Controllers/PostController/ShowTest.php
+++ tests/Feature/Controllers/PostController/ShowTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\Controllers\PostController;
 
+use App\Http\Resources\CommentResource;
 use App\Http\Resources\PostResource;
+use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -19,5 +21,21 @@ class ShowTest extends TestCase
 
         $response = $this->get(route('posts.show', $post->id));
         $response->assertHasResource('post', PostResource::make($post));
+    }
+
+    public function test_should_passes_comments_to_view()
+    {
+        $this->withoutExceptionHandling();
+
+        $posts = Post::factory()->create();
+
+        $comments = Comment::factory(2)
+            ->for($posts)
+            ->create();
+
+        $comments->load('user');
+
+        $response = $this->get(route('posts.show', $posts->id));
+        $response->assertHasPaginatedResource('comments', CommentResource::collection($comments->reverse()));
     }
 }
