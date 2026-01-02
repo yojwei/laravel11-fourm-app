133cd6b9014462942eb35f6c4c2a4f33ec3259e6
Hsieh Chang Wei <jacky790125@gmail.com>
2026-01-02 15:19:37 +0800

    fix(comment): 修正留言顯示樣式，確保正確顯示使用者資料

--- resources/js/Components/Comment.vue
+++ resources/js/Components/Comment.vue
@@ -17,11 +17,11 @@ const deleteComment = () => router.delete(route('comments.destroy', props.commen
         <div class="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
             <img :src="comment.user.profile_photo_url" class="h-10 w-10 rounded-full" />
         </div>
-        <div>
+        <div class="flex-1">
             <p class="mt-1">{{ comment.body }}</p>
             <span class="first-letter:uppercase block pt-1 text-xs text-gray-600">By {{ comment.user.name }} {{
-                relativeDate(comment.created_at) }} ago</span>
-            <div class="block mt-1">
+                relativeDate(comment.created_at) }}</span>
+            <div class="mt-1 text-right empty:hidden">
                 <form @submit.prevent="deleteComment" v-if="comment.can?.delete">
                     <button class="bg-red-500 text-white px-2 py-1 rounded">Delete</button>
                 </form>
