02362f261aeac01a7856ef1aefde81ae75dd2cd1
Hsieh Chang Wei <jacky790125@gmail.com>
2026-01-15 16:12:45 +0800

    refactor(post): 文章建立頁面使用 MarkdownEditor 替換 TextArea

--- resources/js/Pages/Post/Create.vue
+++ resources/js/Pages/Post/Create.vue
@@ -4,7 +4,6 @@ import AppLayout from '@/Layouts/AppLayout.vue';
 import Container from '@/Components/Container.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextArea from "@/Components/TextArea.vue";
 import TextInput from "@/Components/TextInput.vue";
 import InputError from '@/Components/InputError.vue';
 import MarkdownEditor from '@/Components/MarkdownEditor.vue';
@@ -42,8 +41,7 @@ const submitPost = () => postForm.post(route('posts.store'));
                 </div>
                 <div class="mt-2">
                     <InputLabel for="body" class="sr-only" />
-                    <MarkdownEditor v-model="postForm.body" />
-                    <TextArea id="body" v-model="postForm.body" rows="25" class="w-full border rounded-md" />
+                    <MarkdownEditor v-model="postForm.body" placeholder="寫點什麼..." />
                     <InputError :message="postForm.errors.body" class="mt-2" />
                 </div>
                 <div>
