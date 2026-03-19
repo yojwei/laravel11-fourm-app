7620f838dbeb501939bc8f7d51ac9d3fa7035d81
Hsieh Chang Wei <yojwei125@gmail.com>
2026-03-19 13:22:30 +0800

    fix(middleware): 修正信任代理的中介軟體設定

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->trustProxies(at: '*');
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
